7596fdd96784925768e846dd182a19404fa7f121
71359205 <71359205@DESKTOP-J6LME0J>
2018-10-30 20:28:02 +0100

    COMMIT

--- src/Code/Presentation.java
+++ src/Code/Presentation.java
@@ -6,11 +6,39 @@ import java.util.Iterator;
 
 import org.json.*;
 
+import java.util.*;
+
 public class Presentation {					//JSON READER
 	
 	
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(){
 		
+		String jsonFile = "";
+		boolean prunning = false;
+		
+		readJson(jsonFile);
+		prunning = askPrunning();
+		
+	}
+		
+	private static boolean askPrunning() {
+	boolean aux = false;
+	int option = 0;
+	do {
+		
+		System.out.println("Would you like to apply prunning in this search?\n" + 
+				   "1.-Yes\n" + 
+				   "2.-No\n");
+		option = sc.nextInt();
+		if(option == 1) aux = true;
+		else if(option == 2) aux = false;
+	}while(option<1 || option>2);
+	return false;
+}
+
+	private static void readJson(String jsonFile) {
 		JSONParser parser = new JSONParser();
 		
 		try{
@@ -20,5 +48,6 @@ public class Presentation {					//JSON READER
 		} catch(Exception e){
 			e.printStackTrace();
 		}
+	
 	}
 }
